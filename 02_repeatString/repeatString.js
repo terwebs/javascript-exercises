1eaf2b798bd54426ed9357de53a2ccedda5adc03
Webster <terwebs88@gmail.com>
2023-05-15 16:48:03 -0600

    completed repeat string tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number <0 ){
+        return "ERROR"
+    } else {
+        return string.repeat(number)
+    }
+   
 };
 
 // Do not edit below this line
