ab4f662ee8e483188868864d05faea83e2fe74fe
Webster <terwebs88@gmail.com>
2023-05-15 17:21:13 -0600

    completed reverse string tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringMax = string.length - 1;
+    let reverseString = "";
+    for (let i= stringMax; i >= 0; i --){
+        reverseString += string[i]
+    }
+    return reverseString
 };
 
 // Do not edit below this line
