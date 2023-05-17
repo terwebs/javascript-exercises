2b5e711fbfc7a4aceb4d7c1ed3abada7a07551ca
Webster <terwebs88@gmail.com>
2023-05-17 11:41:20 -0600

    completed 'remove from array'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items, ...numbers) {
+    for (let number of numbers){
+        for (let item of items){
+            // console.log(` ${item} = ${number}`, item === number)
+            if (item === number){
+                const index = (items.indexOf(item))
+                items.splice(index, 1)
+            }
+           
+        }
+    }
+    return items;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// removeFromArray([1, 2, 3], "1", 3)
+
+
+
+
+
