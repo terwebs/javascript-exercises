d6957b5b890869c17a117b0dea9b6c55dcef9984
Webster <terwebs88@gmail.com>
2023-05-17 14:47:54 -0600

    completed 'sum all' test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (typeof(a) != 'number' || typeof(b) != 'number'){
+        return "ERROR"
+    } else if (a < 0 || b < 0){
+        return "ERROR"
+    } else {
+        if (a < b){
+            for (let i = a; i <= b; i++){
+                sum += i;
+            }
+        } else {
+            for (let i = b; i <= a; i++){
+                sum += i;
+            }
+        }
+    }  
+    return sum
 };
 
 // Do not edit below this line
