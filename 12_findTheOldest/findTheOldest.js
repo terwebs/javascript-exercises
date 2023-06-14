98e9e30721f65205435383493f5264ea91da567f
Webster <terwebs88@gmail.com>
2023-06-14 17:45:31 -0600

    completed a great map and reduce method  exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((oldest, currentPerson) => {
+    if(checkAge(oldest) > checkAge(currentPerson)){
+     return oldest
+    } else {
+     return currentPerson
+    }
+ })
 };
+
+const checkAge = function(person){
+    let age = 0;
+    const currentYear = new Date().getFullYear()
+    if(!person.yearOfDeath){
+        age = currentYear - person.yearOfBirth
+    } else {
+        age = person.yearOfDeath - person.yearOfBirth;
+    }
+    return age
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
