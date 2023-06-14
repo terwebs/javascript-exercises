da7170f43da29a54912c56a9810570bcfbb4925c
Webster <terwebs88@gmail.com>
2023-06-14 14:00:00 -0600

    completed calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+let total = 0; 
+  for(let number of numbers){
+    parseInt(number)
+    total += number;
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1; 
+  for(let number of numbers){
+    parseInt(number)
+    total *= number;
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let total = a;
+	for (let i=1; i < b; i++){
+     total *= a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+	for (let i=1; i<=number; i++){
+    total *= i;
+  } return total
 };
 
 // Do not edit below this line
