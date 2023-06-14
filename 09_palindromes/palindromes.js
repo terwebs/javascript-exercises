5ed6d2bce303f137278cc4502d6057a1c5072313
Webster <terwebs88@gmail.com>
2023-06-14 14:42:25 -0600

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const arrayWord = word.toLowerCase().replace(/[^a-z]/g, "").split("");
+    for (let i=0; i <= arrayWord.length; i++){
+        //console.log(arrayWord[i], arrayWord[arrayWord.length-(i+1)])
+        if(arrayWord[i] != arrayWord[arrayWord.length-(i+1)]){
+            return false
+        }
+    } return true;
 };
 
 // Do not edit below this line
