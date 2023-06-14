b91eff3c0aee8b2982f3608f373fca29fe0476cc
Webster <terwebs88@gmail.com>
2023-06-14 15:18:43 -0600

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    let num1 = 0;
+    let num2 = 1;
+    let temp = 0;
+    if (parseInt(count) < 0) {
+        return "OOPS"
+    }
+    for (let i = 1; i <= parseInt(count); i++){
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp
+    }
+    return num1
+    //starting sequence in 1 instead of 0
 };
 
 // Do not edit below this line
